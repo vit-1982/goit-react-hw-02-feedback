f5845fd3ba46ae12760269ca96bfeb5e036e58e0
Vitalii Zelenyi <vzntst@gmail.com>
2020-10-11 21:34:37 +0300

    Updates

---
+++ precache-manifest.5c62ee497b4077bb510d5b3f017fc44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1723458e473ffc9772cf911c067c0fb",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "0ec2a26a19666e7296ac",
+    "url": "/goit-react-hw-02-feedback/static/css/main.76136d86.chunk.css"
+  },
+  {
+    "revision": "63c5f8a88f06798a4da2",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8cd08abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8cd08abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec2a26a19666e7296ac",
+    "url": "/goit-react-hw-02-feedback/static/js/main.6f7d26e1.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
